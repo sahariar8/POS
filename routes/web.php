836a9e1844d2bc1895b8a312afe9618d02dae261
shahariar <shahariar@inneed.cloud>
2025-01-03 00:45:44 +0600

    create product route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,4 @@ Route::get('/', function () {
 
 
 Route::get('/category',[CategoryController::class,'index']);
+Route::get('/product',[ProductController::class,'index']);
