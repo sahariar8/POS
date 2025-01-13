253295aa7f54f898b897a27e3fda548e2780de71
sahariar8 <sahariaralam8@gmail.com>
2025-01-13 17:14:09 +0600

    feat:OTP

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,10 @@ class User extends Authenticatable
         'password',
     ];
 
+    protected $attributes = [
+        'otp'=>'0'
+    ];
+
     /**
      * The attributes that should be hidden for serialization.
      *
